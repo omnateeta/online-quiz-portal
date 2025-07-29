fe0c8914461f032bf3add277350d58b69dd91a92
Omnateeta V Unnimath <omnateeta3@gmail.com>
2025-07-29 17:42:00 +0530

    chore: add startup log for Render deployment debugging

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,17 @@ import userRoutes from './routes/user.routes.js';
 import adminRoutes from './routes/admin.routes.js';
 import { errorHandler } from './middleware/error.middleware.js';
 
+console.log('=== Server.js starting ===');
+
+process.on('uncaughtException', err => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+process.on('unhandledRejection', err => {
+  console.error('Unhandled Rejection:', err);
+  process.exit(1);
+});
+
 dotenv.config();
 
 const app = express();
